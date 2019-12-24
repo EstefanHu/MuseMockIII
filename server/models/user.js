38f2b706ad6fef56858c59f3369fea26924bc220
Estefan Hu <estefanhu074@gmail.com>
2019-12-24 12:45:44 -0800

    changeing machines

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const postSchema = require('./post');
+const postSchema = require('./post').schema;
 
 const userSchema = new Schema({
     firstName: String,
--- server/test/base.js
+++ server/test/base.js
@@ -21,42 +21,42 @@ describe('Base User tests', () => {
     });
   });
 
-  it('Updating one User', done => {
-    User.findOneAndUpdate({firstName: 'Estefan'}, {firstName: 'Justin'}).then(() => {
-      user.findOne({_id: newUser._id}).then(result => {
-        assert(result.firstName === 'Justin');
-        done();
-      });
-    });
-  });
+  // it('Updating one User', done => {
+  //   User.findOneAndUpdate({firstName: 'Estefan'}, {firstName: 'Justin'}).then(() => {
+  //     user.findOne({_id: newUser._id}).then(result => {
+  //       assert(result.firstName === 'Justin');
+  //       done();
+  //     });
+  //   });
+  // });
 
-  it('Increment User credibility by one', done => {
-    User.updateMany({}, {$inc: {credibility: 1}}).then(() => {
-      User.findOne({firstName: 'Justin'}).then(record => {
-        assert(record.credibility === 1);
-        done();
-      });
-    });
-  });
+  // it('Increment User credibility by one', done => {
+  //   User.updateMany({}, {$inc: {credibility: 1}}).then(() => {
+  //     User.findOne({firstName: 'Justin'}).then(record => {
+  //       assert(record.credibility === 1);
+  //       done();
+  //     });
+  //   });
+  // });
 
-  it('Adding post to User', done => {
-    User.findOne({fistName: 'Justin'}).then(record => {
-      record.posts.push({title: 'Title', description: 'description', content: 'this is some content'});
-      record.save().then(() => {
-        User.findOne({firstName: 'Justin'}).then(record => {
-          assert(record.posts.length === 1);
-          done();
-        });
-      });
-    });
-  });
+  // it('Adding post to User', done => {
+  //   User.findOne({fistName: 'Justin'}).then(record => {
+  //     record.posts.push({title: 'Title', description: 'description', content: 'this is some content'});
+  //     record.save().then(() => {
+  //       User.findOne({firstName: 'Justin'}).then(record => {
+  //         assert(record.posts.length === 1);
+  //         done();
+  //       });
+  //     });
+  //   });
+  // });
 
-  it('Deleting User from Database', done => {
-    User.findOneAndRemove({firstName: 'Justin'}).then(() => {
-      User.findOne({firstName: 'Justin'}).then(result => {
-        assert(result === null);
-        done();
-      });
-    });
-  });
+  // it('Deleting User from Database', done => {
+  //   User.findOneAndRemove({firstName: 'Justin'}).then(() => {
+  //     User.findOne({firstName: 'Justin'}).then(result => {
+  //       assert(result === null);
+  //       done();
+  //     });
+  //   });
+  // });
 });
