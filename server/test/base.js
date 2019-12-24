cd79dfc79332520b623ed2df2e4d9a052fb1c889
Estefan Hu <estefanhu074@gmail.com>
2019-12-23 17:14:52 -0800

    Updated tests

---
+++ server/test/base.js
@@ -0,0 +1,62 @@
+const assert = require('assert');
+const User = require('../models/user');
+
+describe('Base User tests', () => {
+
+  let newUser;
+
+  it('Saving User to the database', done => {
+    newUser = new User({
+      firstName: 'Estefan',
+      lastName: 'Hu',
+      email: 'estefanhu@gmail.com',
+      credibility: '0',
+      posts: [],
+      archive: []
+    });
+
+    newUser.save().then(() => {
+      assert(user.isNew  === false);
+      done();
+    });
+  });
+
+  it('Updating one User', done => {
+    User.findOneAndUpdate({firstName: 'Estefan'}, {firstName: 'Justin'}).then(() => {
+      user.findOne({_id: newUser._id}).then(result => {
+        assert(result.firstName === 'Justin');
+        done();
+      });
+    });
+  });
+
+  it('Increment User credibility by one', done => {
+    User.updateMany({}, {$inc: {credibility: 1}}).then(() => {
+      User.findOne({firstName: 'Justin'}).then(record => {
+        assert(record.credibility === 1);
+        done();
+      });
+    });
+  });
+
+  it('Adding post to User', done => {
+    User.findOne({fistName: 'Justin'}).then(record => {
+      record.posts.push({title: 'Title', description: 'description', content: 'this is some content'});
+      record.save().then(() => {
+        User.findOne({firstName: 'Justin'}).then(record => {
+          assert(record.posts.length === 1);
+          done();
+        });
+      });
+    });
+  });
+
+  it('Deleting User from Database', done => {
+    User.findOneAndRemove({firstName: 'Justin'}).then(() => {
+      User.findOne({firstName: 'Justin'}).then(result => {
+        assert(result === null);
+        done();
+      });
+    });
+  });
+});
--- server/test/demo.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-
-describe('some demo tests', () => {
-    it('adds two numbers together', done => {
-        assert(2 + 3 === 5);
-        done();
-    });
-});
