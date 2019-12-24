5ab31eb356b24a86f09b24876d6e07f939fe4b6f
Estefan Hu <estefanhu074@gmail.com>
2019-12-24 14:56:48 -0800

    beginning debugging

--- server/models/hub.js
+++ server/models/hub.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const userSchema = require('./user').schema;
+const postSchema = require('./post').schema;
 
 const hubSchema = new Schema({
     name: String,
-    districtId: String,
-    neighborhoodId: String
+    admins: [userSchema],
+    members: [userSchema],
+    posts: [postSchema]
 });
 
 module.exports = mongoose.model('Hub', hubSchema);
--- server/test/setup.js
+++ server/test/setup.js
@@ -1,27 +1,80 @@
-// const assert = require('assert');
-// const City = require('../models/city');
-// const District = require('../models/district');
-// const Hub = require('../models/hub');
-// const User = require('../models/user');
+const assert = require('assert');
+const mongoose = require('mongoose');
+const City = mongoose.model('City', '../models/city');
+// const District = mongoose.model('../models/district');
+// const Neighborhood = mongoose.model('../models/neighborhood');
+// const Hub = mongoose.model('../models/hub');
+// const User = mongoose.model('../models/user');
 
-// describe('Setting up application documents', () => {
+describe('Setting up application documents', () => {
 
-//     it('City document established', done => {
-//         let newCity = new City({
-//             name: 'Seatlle'
-//         });
-//         CountQueuingStrategy.findOneAndUpdate({})
-//     });
+    it('City document established', done => {
+        let newCity = new City({
+            name: 'Seatle',
+            districts: [],
+            posts: []
+        });
 
-//     it('District document established', done => {
-//         let newDistrict = new District({});
-//     });
+        newCity.save().then(() => {
+            assert(newCity.isNew === false);
+            done();
+        });
+    });
 
-//     it('Hub document established', done => {
+    it('District document established', done => {
+        let newDistrict = new District({
+            name: 'Four',
+            neighborhoods: [],
+            hubs: [],
+            posts: []
+        });
 
-//     });
+        newDistrict.save().then(() => {
+            assert(newDistrict.isNew === false);
+            done();
+        });
+    });
 
-//     it('User document established', done => {
+    it('Neighborhood document established', done => {
+        let newNeighborhood = new Neighborhood({
+            name: 'University District',
+            hubs: [],
+            posts: []
+        });
 
-//     });
-// });
+        newNeighborhood.save().then(() => {
+            assert(newNeighborhood.isNew === false);
+            done();
+        });
+    });
+
+    it('Hub document established', done => {
+        let newHub = new Hub({
+            name: 'University of Washington',
+            admins: [],
+            members: [],
+            posts: []
+        });
+
+        newHub.save().then(() => {
+            assert(newHub.isNew === false);
+            done();
+        });
+    });
+
+    it('User document established', done => {
+        let newUser = new User({
+            firstName: 'Estefan',
+            lastName: 'Hu',
+            email: 'estefanhu@gmail.com',
+            password: 'password',
+            credibility: 0,
+            posts: []
+        });
+
+        newUser.save().then(() => {
+            assert(newUser.isNew === false);
+            done();
+        });
+    });
+});
