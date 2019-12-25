c970918a05209b37d329666d46135e0505bed028
Estefan Hu <estefanhu074@gmail.com>
2019-12-25 14:01:32 -0800

    Connected Components to backend query

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import ApolloClient from 'apollo-boost';
 import {ApolloProvider} from 'react-apollo';
 import './App.css';
 
+import Dashboard from './components/dashboard';
+
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql'
 });
@@ -12,6 +14,7 @@ function App() {
     <ApolloProvider client={client}>
       <div className="App">
         <h1>Hello Muse</h1>
+        <Dashboard/>
       </div>
     </ApolloProvider>
   );
