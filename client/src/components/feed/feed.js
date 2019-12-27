e3b45db0bbc75d75be81e1dc5ad323d1220262fe
Estefan Hu <estefanhu074@gmail.com>
2019-12-27 15:54:58 -0800

    Updated user model

---
+++ client/src/components/feed/feed.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Post from './post';
+
+class Feed extends Component {
+    render() {
+        return (
+            <section>
+                
+            </section>
+        )
+    }
+}
+
+export default Feed;
--- server/models/user.js
+++ server/models/user.js
@@ -3,13 +3,39 @@ const Schema = mongoose.Schema;
 const postSchema = require('./post').schema;
 
 const userSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    password: String,
+    firstName: {
+        type: String,
+        require: true,
+        trim: true,
+        minlength: 2
+    },
+    lastName: {
+        type: String, 
+        require: true,
+        trim: true,
+        minlength: 2
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true,
+        trim: true,
+        minlength: 5
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 8
+    },
+    createdAt: {
+        type: Date,
+        require: true
+    },
     credibility: Number,
     posts: [postSchema],
-    archive: [postSchema]
+    archive: [postScfhema]
+}, {
+    timestamps: true
 });
 
 module.exports = mongoose.model('User', userSchema);
