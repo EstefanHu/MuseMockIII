7d9ba8cac926d3acd04379cb666c0e519204eb60
Estefan Hu <estefanhu074@gmail.com>
2019-12-27 18:16:45 -0800

    Updated post model

--- server/models/post.js
+++ server/models/post.js
@@ -2,9 +2,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
-    title: String,
-    description: String,
-    content: String
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    content: {
+        type: String,
+        requried: true,
+        trim: true
+    }
+},{
+    timestamps: true
 });
 
 module.exports = mongoose.model('Post', postSchema);
