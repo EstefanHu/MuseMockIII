23dcd110bbf0d311d0d88152993a66fed3e8c67c
Estefan Hu <estefanhu074@gmail.com>
2019-12-27 18:32:07 -0800

    Debugged rendering

--- client/src/components/create/create.js
+++ client/src/components/create/create.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import Write from './write';
+
 class Create extends Component {
     render() {
         return (
             <main>
                 <h1>Hello from Create</h1>
+                <Write />
             </main>
         )
     }
