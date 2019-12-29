70a65b9746bb65c7923e1f44ec6594d8508558f4
Estefan Hu <estefanhu074@gmail.com>
2019-12-28 20:17:50 -0800

    installed setting markup

---
+++ client/src/components/settings/settings.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Settings extends Component {
+    render() {
+        return(
+            <main>
+                <h1>Hello from Settings</h1>
+            </main>
+        )
+    }
+}
+
+export default Settings;
