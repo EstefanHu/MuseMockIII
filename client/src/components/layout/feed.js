55d6974582aeda81c217e1804ced2f8f8ec5c123
Estefan Hu <estefanhu074@gmail.com>
2020-01-03 16:53:09 -0800

    Added feed boilerplate

---
+++ client/src/components/layout/feed.js
@@ -0,0 +1,20 @@
+
+import React, { Component } from 'react';
+
+class Feed extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <>
+                <h1>hello feed</h1>
+            </>
+        )
+    }
+}
+
+export default Feed;
