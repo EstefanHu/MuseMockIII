a2d6552a775163091ea37689a8c77c090366218c
Estefan Hu <estefanhu074@gmail.com>
2020-01-03 17:23:28 -0800

    Updated tag types

--- client/src/components/layout/feed.js
+++ client/src/components/layout/feed.js
@@ -5,11 +5,11 @@ import Post from './post';
 class Feed extends Component {
     render() {
         return (
-            <>
+            <section>
                 {this.props.posts.map(item => (
                     <Post post={item} key={item.id} />
                 ))}
-            </>
+            </section>
         )
     }
 }
--- client/src/components/layout/post.js
+++ client/src/components/layout/post.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 
-class Post extends Component {
-    render() {
-        return (
-            <article>
-                <h3>{this.props.post.title}</h3>
-                <p>{this.props.post.description}</p>
-            </article>
-        )
-    }
+function Post(props) {
+    return (
+        <article>
+            <h3>{props.post.title}</h3>
+            <p>{props.post.description}</p>
+        </article>
+    )
 }
 
 export default Post;
