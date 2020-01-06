72e1d8d0e7c8605d1dc3d044f6f86623b10e50b7
Estefan Hu <estefanhu074@gmail.com>
2020-01-06 10:10:08 -0800

    finished base styles for post

--- client/src/components/layout/post.js
+++ client/src/components/layout/post.js
@@ -5,12 +5,12 @@ function Post(props) {
         <article>
             <span>
                 <p>
-                    <a href="javascript:filterGenre()">{props.post.genre}</a> by {props.post.author}
+                    <a href="javascript:filterGenre()" className="genre">{props.post.genre}</a> by {props.post.author}
                 </p>
                 <p>Cred: {props.post.credibility}</p>
             </span>
-            <h2>{props.post.title}</h2>
-            <p>{props.post.description}</p>
+            <h2><a href="/post/{props.post.id}">{props.post.title}</a></h2>
+            <p className="description">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{props.post.description}</p>
         </article>
     )
 }
