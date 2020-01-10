4f3b24fe20d50cca29ba26124e29549391392872
Estefan Hu <estefanhu074@gmail.com>
2020-01-10 14:07:27 -0800

    connected createPublicaiton route

--- server/routes/create.js
+++ server/routes/create.js
@@ -4,4 +4,12 @@ router.route('/').get((req, res) => {
   res.json({"hello": "World"});
 });
 
+router.route('/createPublication').get((req, res) => {
+  try {
+    res.json({"Hello": "fromCreatePublicaiton"});
+  } catch(err) {
+    res.type('text').status(500).send('Error: ' + err);
+  }
+})
+
 module.exports = router;
