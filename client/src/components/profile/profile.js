c8936392ccd8b776f649c87a42cff06925a2dce6
Estefan Hu <estefanhu074@gmail.com>
2020-01-13 10:52:33 -0800

    removed testing form

--- client/src/components/profile/profile.js
+++ client/src/components/profile/profile.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 
-import Form from '../layout/form';
 
 class Profile extends Component {
     render() {
         return (
             <main>
                 <h1>Hello from Profile</h1>
-                <Form />
             </main>
         )
     }
