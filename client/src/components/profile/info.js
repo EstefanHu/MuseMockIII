1391a4a74090e73db6ba54699e6f72b866f52db3
Estefan Hu <estefanhu074@gmail.com>
2020-01-14 12:50:59 -0800

    updated spacing

--- client/src/components/profile/info.js
+++ client/src/components/profile/info.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 const Info = props => {
   return (
-    <section className="left" >
+    <section className="left" style={primary} >
       <p>{props.user.firstName} {props.user.lastName}</p>
     </section>
   )
+}
+
+const primary = {
+  backgroundColor: 'white',
+  boxShadow: '12px 12px 12px 0 rgba(0, 0, 0, 0.05)'
 }
 
 export default Info;
