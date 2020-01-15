c9be5904841d2aa8283b0d370181f87f705625f2
Estefan Hu <estefanhu074@gmail.com>
2020-01-14 16:57:57 -0800

    updated navigation styles

--- client/src/components/home/navigation.js
+++ client/src/components/home/navigation.js
@@ -3,11 +3,27 @@ import React from 'react';
 const Navigation = props => {
   return (
     <section className="left">
+      <span style={ navItem }><h3 style={ navHead } >Sectors</h3></span>
       {props.sectors.map(sector => (
-        <span>{sector}</span>
+        <span style={ navItem }>{sector}</span>
       ))}
     </section>
   );
+}
+
+const navItem = {
+  display: 'block',
+  textAlign: 'center',
+  backgroundColor: 'white',
+  borderRadius: '5px',
+  marginBottom: '10px',
+  fontSize: '1.2rem',
+  padding: '10px 5px',
+  boxShadow: '12px 12px 12px 0 rgba(0, 0, 0, 0.05)'
+}
+
+const navHead = {
+  textDecoration: 'underline'
 }
 
 export default Navigation;
