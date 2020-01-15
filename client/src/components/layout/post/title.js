624f0d69e16cd58e28d97e90b663ec0518a7eecb
Estefan Hu <estefanhu074@gmail.com>
2020-01-15 14:41:21 -0800

    Updated hover functionality

--- client/src/components/layout/post/title.js
+++ client/src/components/layout/post/title.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
 const Title = props => {
+  const onEnter = e => {
+    e.target.style.color="grey"
+  }
+
+  const onLeave = e => {
+    e.target.style.color="rgb(61, 61, 61)"
+  }
+
   return (
-    <h2>{ props.title }</h2>
+    <h2
+      onMouseEnter={ onEnter }
+      onMouseLeave={ onLeave }
+    >{ props.title }</h2>
   )
 }
 
