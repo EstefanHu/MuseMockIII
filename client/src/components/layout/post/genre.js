77f60958502726a26bd0dfca6b63a2a837a7ec2d
Estefan Hu <estefanhu074@gmail.com>
2020-01-15 14:45:38 -0800

    Inlined hover styles

--- client/src/components/layout/post/genre.js
+++ client/src/components/layout/post/genre.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
 const Genre = props => {
+  const onEnter = e => {
+    e.target.style.color="rgb(173, 173, 173)";
+    e.target.style.textDecoration="underline";
+  }
+
+  const onLeave = e => {
+    e.target.style.color="grey";
+    e.target.style.textDecoration="none";
+  }
+
   return (
-    <a href="/" style={ genre }>{ props.genre }</a>
+    <a href="/"
+    style={ genre }
+    onMouseEnter={ onEnter }
+    onMouseLeave={ onLeave }
+    >{ props.genre }</a>
   )
 }
 
