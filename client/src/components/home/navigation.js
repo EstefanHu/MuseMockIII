773a34991625f344ee3af62cc94e101dcbbf070d
Estefan Hu <estefanhu074@gmail.com>
2020-01-14 19:03:13 -0800

    Completed navigation scrolling

--- client/src/components/home/navigation.js
+++ client/src/components/home/navigation.js
@@ -3,27 +3,38 @@ import React from 'react';
 const Navigation = props => {
   return (
     <section className="left">
-      <span style={ navItem }><h3 style={ navHead } >Sectors</h3></span>
-      {props.sectors.map(sector => (
-        <span style={ navItem }>{sector}</span>
-      ))}
+      <div style={ scroll } >
+        <span style={ navItem }><h3 style={ navHead } >Sectors</h3></span>
+        {props.sectors.map(sector => (
+          <button style={ navItem }>{sector}</button>
+        ))}
+      </div>
     </section>
   );
+}
+
+const scroll = {
+  position: 'fixed',
+  width: '240px',
+  height: '100%',
+  overflow: 'scroll'
+}
+
+const navHead = {
+  textDecoration: 'underline'
 }
 
 const navItem = {
   display: 'block',
   textAlign: 'center',
   backgroundColor: 'white',
+  border: 'none',
   borderRadius: '5px',
   marginBottom: '10px',
   fontSize: '1.2rem',
   padding: '10px 5px',
-  boxShadow: '12px 12px 12px 0 rgba(0, 0, 0, 0.05)'
-}
-
-const navHead = {
-  textDecoration: 'underline'
+  boxShadow: '12px 12px 12px 0 rgba(0, 0, 0, 0.05)',
+  width: '100%',
 }
 
 export default Navigation;
--- client/src/components/profile/credibility.js
+++ client/src/components/profile/credibility.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 const Credibility = props => {
   return (
-    <p style={ cred }>{ props.credibility }</p>
+    <p style={ cred }>Credibility: { props.credibility }</p>
   )
 }
 
 const cred = {
+  marginTop: '10px',
+  marginBottom: '5px',
+  fontSize: '.9rem'
 }
 
 export default Credibility;
